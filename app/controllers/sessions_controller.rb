c739fbe3fbb4399430b4929460ca0d1b08c57ac7
Alexander Schofield <alschofield93@gmail.com>
2015-12-17 14:32:02 -0500

    define session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,29 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
+    if params[:login_selector] == "student"
+      @student = Student.find_by({email: params[:email]}).try(:authenticate, params[:password])
+      sessions[:student_id] = @student.id
+    elsif params[:login_selector] == "instructor"
+      @instructor = Instructor.find_by({email: params[:email]}).try(:authenticate, params[:password])
+      sessions[:instructor_id] = @instructor.id
+    elsif params[:login_selector] == "producer"
+      @producer = Producer.find_by({email: params[:email]}).try(:authenticate, params[:password])
+      sessions[:producer_id] = @producer.id
+    end  
   end
 
   def destroy
+    if sessions[:student_id]
+      sessions[:student_id] = nil
+    elsif sessions[:instructor_id]
+      sessions[:instructor_id] = nil
+    elsif sessions[:producer_id]
+      sessions[:producer_id] = nil
+    end
   end
+
 end
