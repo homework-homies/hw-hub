aa64054f11b2a5b532b30d9686e83cae8547cca1
Thais Pulliam <thaispulliam@gmail.com>
2015-12-17 15:19:07 -0500

    Finish up queries on student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,37 @@
 class StudentsController < ApplicationController
+
+  def show
+    @student = Student.find(params[:id])
+    @instructors = Cohort.find(@student.cohort_id).instructors
+    @assignments = Cohort.find(@student.cohort_id).assignments
+    @submissions = @student.submissions
+    @gists = Cohort.find(@student.cohort_id).gists
+    @new_submission = Submission.new
+
+  end
+
   def new
-    
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(student_params)
+    redirect_to "/login"
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
-  def show
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to "/"
   end
 
-  def delete
+private
+  def student_params
+      params.require(:student).permit(:name, :email, :password, :password_confirmation)
   end
+
 end  
