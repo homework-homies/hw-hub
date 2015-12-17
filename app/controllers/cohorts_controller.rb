ca30f3f5a028e6ab3003d514c8bc7ef1cf41bce8
Basso Basso <taylorpbasso@gmail.com>
2015-12-17 14:49:32 -0500

    added content to the cohort controller

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,7 +1,16 @@
 class CohortsController < ApplicationController
   def create
+    @cohort = Cohort.create({
+      cohort_name: params["cohort_name"],
+      producer_id: params["producer_id"],
+      start_on: params["start_on"],
+      end_on: params["end_on"]
+    })
+    redirect_to @cohort
   end
 
   def show
+    @cohort = Cohort.find(params[:id])
   end
+
 end
