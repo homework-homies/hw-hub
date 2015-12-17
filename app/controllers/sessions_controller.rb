e7821039829ec77a23c28960a9f86831db37aaaa
Alexander Schofield <alschofield93@gmail.com>
2015-12-17 15:08:42 -0500

    adds redirects to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,15 @@ class SessionsController < ApplicationController
     if params[:login_selector] == "student"
       @student = Student.find_by({email: params[:email]}).try(:authenticate, params[:password])
       sessions[:student_id] = @student.id
+      redirect_to @student
     elsif params[:login_selector] == "instructor"
       @instructor = Instructor.find_by({email: params[:email]}).try(:authenticate, params[:password])
       sessions[:instructor_id] = @instructor.id
+      redirect_to @instructor
     elsif params[:login_selector] == "producer"
       @producer = Producer.find_by({email: params[:email]}).try(:authenticate, params[:password])
       sessions[:producer_id] = @producer.id
+      redirect_to @producer
     end  
   end
 
@@ -22,6 +25,7 @@ class SessionsController < ApplicationController
       sessions[:instructor_id] = nil
       sessions[:producer_id] = nil
     end
+    redirect_to '/'
   end
 
 end
