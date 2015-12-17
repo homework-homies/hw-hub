994919dad48fafe496ce967d1007f492d075af10
Jess Lee <jessleenyc@gmail.com>
2015-12-17 14:52:51 -0500

    updates seed file so all relationships work

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,6 @@ assignment1 = Assignment.create({ title: "assignment1", prompt_link:"assignment1
 assignment2 = Assignment.create({ title: "assignment2", prompt_link:"assignment2 link", assigned_on: "12345903"})
 assignment3 = Assignment.create({ title: "assignment3", prompt_link:"assignment3 link", assigned_on: "12345903"})
 
-<<<<<<< HEAD
 #join instructors & cohorts
 cohort1.instructors.push(instructor1)
 cohort2.instructors.push(instructor1)
