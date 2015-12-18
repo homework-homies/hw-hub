33e943fb00794c3788bd55979562ec3835927ed4
Jess Lee <jessleenyc@gmail.com>
2015-12-18 11:06:27 -0500

    Assignments Controller new and create

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,18 @@
 class AssignmentsController < ApplicationController
 
+  def new
+    @assignment = Assignment.new
+    @instructor = Instructor.find(sessions[:instructor_id])
+  end
+
   def create
     @assignment = Assignment.create(assignment_params)
+    @instructor = Instructor.find(sessions[:instructor_id])
+    @assignment.instructor.push(@instructor)
+    @cohort = Cohort.find_by(cohort_name: params[:cohort_selector]) 
+    @assignment.cohort.push(@cohort)
+    @assignment.assigned_on = Date.new
+    @assignment.save
     redirect_to instructor_path(sessions[:instructor_id]) if sessions[:instructor_id]
   end
 
