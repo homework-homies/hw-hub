03a1cdf4e2c69c57980881ac6cc2493d6441715a
Jess Lee <jessleenyc@gmail.com>
2015-12-18 13:50:29 -0500

    adds completetion rate method

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,4 +14,16 @@ class Student < ActiveRecord::Base
   has_many :submissions
   has_many :assignments, through: :submissions
   belongs_to :cohort
+
+
+  def completion_rate 
+    submissions = self.submissions
+    num_complete = 0;
+    submissions.each do |submission|
+      if submission.grade_status == "complete"
+        num_complete ++;
+      end
+    end
+    completion_rate = (num_complete/submissions) * 100
+  end
 end
