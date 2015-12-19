94943afa05a81cccfe7bc4a8bcd6d791963996b6
Jess Lee <jessleenyc@gmail.com>
2015-12-19 13:45:07 -0500

    adds email and password validations

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -10,6 +10,8 @@
 
 class Instructor < ActiveRecord::Base
   has_secure_password
+  validates :email, uniqueness: true 
+  validates :password, length: { minimum: 6 }
   has_many :gists
   has_many :assignments
   has_and_belongs_to_many :cohorts
--- app/models/producer.rb
+++ app/models/producer.rb
@@ -10,5 +10,7 @@
 
 class Producer < ActiveRecord::Base
   has_secure_password
+  validates :email, uniqueness: true 
+  validates :password, length: { minimum: 6 }
   has_many :cohorts
 end
