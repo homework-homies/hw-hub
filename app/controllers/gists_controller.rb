85486e558cfa41a512ad524a875c3784e5b9dd31
Thais Pulliam <thaispulliam@gmail.com>
2015-12-20 21:02:08 -0500

    Fix the New Gist Form that was not working

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -3,12 +3,12 @@ class GistsController < ApplicationController
   def create
     @gist = Gist.create(gist_params)
 
-    @gist.cohort = Cohort.find_by(cohort_name: params[:cohort_select])
-    @gist.save
-    
-    @gist.cohort.students.each do |student|
+    if @gist.cohort.students.any?
+      @gist.cohort.students.each do |student|
         GistMailer.gist_email(student.email).deliver_now
-    end 
+      end 
+    end
+    
       redirect_to @gist
   end
 
